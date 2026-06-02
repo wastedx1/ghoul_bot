7dedfcf6fc6fc13c1baa98d4429154c833d1028a
wastedx1 <138141517+wastedx1@users.noreply.github.com>
2026-06-02 18:00:59 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Чистит чилен гулям",
+        name: "Играет с волосатым кагуне",
         // Activity type number (0 = Playing).
         type: 0, 
       },
