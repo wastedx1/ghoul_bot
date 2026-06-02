d1429e1f262897bfb1f52ff24c4d258edac2fa47
wastedx1 <138141517+wastedx1@users.noreply.github.com>
2026-06-02 18:03:04 +0300

    Update bot activity name in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Играет с волосатым кагуне",
+        name: "Наблюдает за районом",
         // Activity type number (0 = Playing).
         type: 0, 
       },
